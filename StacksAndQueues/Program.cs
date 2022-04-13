cfb16b4e26591ddd1cfc6cd978e16c76b7a70524
Arun37-Kumar <arun007624@gmail.com>
2022-04-13 17:51:23 +0530

    commit

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack list = new Stack();
+            list.push(30);
+            list.push(56);
+            list.push(70);
+            list.Display();
+            Console.WriteLine();
+            list.Peek();
+            list.Pop();
+            list.Display();
+
+            //Queue queue = new Queue();
+            //queue.Enqueue(56);
+            //queue.Enqueue(30);
+            //queue.Enqueue(70);
+            //queue.Display();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StacksAndQueues/Stack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Stack
+    {
+        public Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+
+        public void push(int value)
+        {
+            Node node = new Node(value);
+            if(this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed in stack.", value);
+        }
+
+        //To display the elements in the stack
+        public void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        //To check for the element
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty!");
+                return;
+            }
+            Console.WriteLine("{0} is at the top of the stack.", this.top.data);
+        }
+
+        //Pop opertions in stacks
+        public void Pop()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack is empty!");
+                return;
+            }
+            Console.WriteLine("Value popped is {0}",this.top.data);
+            this.top = this.top.next;
+        }
+
+        //Check for empty Stack
+        public void isEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+
+    }
+}
